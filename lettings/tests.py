3f6a31dab1b3ec3fd4f6eeb824dbe51361db2e25
TheHarryPop <winkelquentin@gmail.com>
2022-04-05 14:27:28 +0200

    Test pipeline prise en compte test_retrieve

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Letting, Address
+
+
+class TestLettingsView:
+    @pytest.mark.django_db
+    def test_retrieve_letting_index(self, client):
+        address_1 = Address.objects.create(number="1", street="la joie", city="south park",
+                                           state="florida", zip_code="76560", country_iso_code="222")
+        letting_1 = Letting.objects.create(title="Maison", address=address_1)
+        response = client.get(reverse('lettings_index'))
+
+        assert response.status_code == 200
+        assert "Maison" in str(response.content)
