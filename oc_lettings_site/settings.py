dc6ac7f7d6a49c573b0b738801aa2bb5c5b583ad
TheHarryPop <winkelquentin@gmail.com>
2022-04-12 13:56:53 +0200

    Test deployment Heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -107,18 +107,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
 
 django_heroku.settings(locals())
