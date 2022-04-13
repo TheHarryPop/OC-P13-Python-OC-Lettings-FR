9a555352833006743b18981944a68d86efd2da3a
TheHarryPop <winkelquentin@gmail.com>
2022-04-13 15:27:46 +0200

    Deployment Heroku ok

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,7 +11,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_random_secret_key()
-# SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
