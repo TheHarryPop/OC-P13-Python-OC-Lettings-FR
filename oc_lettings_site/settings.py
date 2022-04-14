e697cfb8c6ea52eb24c2d1e4ba8c3c01b8577e8c
TheHarryPop <winkelquentin@gmail.com>
2022-04-14 11:36:26 +0200

    load dotenv .env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,13 +13,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 # SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -119,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
 
 sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_KEY"),
+    dsn=str(os.environ.get('SENTRY_KEY')),
     # dsn="https://40cbf7c8452f406581e3eac73e1cf19c@o1202833.ingest.sentry.io/6328281",
     integrations=[DjangoIntegration()],
 
